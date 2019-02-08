4673e1ef43bea2c3512a4c404c0da2eda012d00f
Johan Brinch <johan@pleo.io>
2019-02-08 13:20:13 +0100

    Add some comments

--- pleo-antaeus-rest/src/main/kotlin/io/pleo/antaeus/rest/AntaeusRest.kt
+++ pleo-antaeus-rest/src/main/kotlin/io/pleo/antaeus/rest/AntaeusRest.kt
@@ -1,3 +1,7 @@
+/*
+    Configures the rest app along with basic exception handling and URL endpoints.
+ */
+
 package io.pleo.antaeus.rest
 
 import io.javalin.Javalin
@@ -19,22 +23,28 @@ class AntaeusRest (
         app.start(7000)
     }
 
+    // Set up Javalin rest app
     private val app = Javalin
         .create()
         .apply {
+            // InvoiceNotFoundException: return 404 HTTP status code
             exception(InvoiceNotFoundException::class.java) { _, ctx ->
                 ctx.status(404)
             }
+            // Unexpected exception: return HTTP 500
             exception(Exception::class.java) { e, _ ->
                 logger.error(e) { "Internal server error" }
             }
+            // On 404: return message
             error(404) { ctx -> ctx.json("not found") }
         }
 
     init {
+        // Set up URL endpoints for the rest app
         app.routes {
            path("rest") {
                // Route to check whether the app is running
+               // URL: /rest/health
                get("health") {
                    it.json("ok")
                }
@@ -42,20 +52,24 @@ class AntaeusRest (
                // V1
                path("v1") {
                    path("invoices") {
+                       // URL: /rest/v1/invoices
                        get {
                            it.json(invoiceService.fetchAll())
                        }
 
+                       // URL: /rest/v1/invoices/{:id}
                        get(":id") {
                           it.json(invoiceService.fetch(it.pathParam("id").toInt()))
                        }
                    }
 
                    path("customers") {
+                       // URL: /rest/v1/customers
                        get {
                            it.json(customerService.fetchAll())
                        }
 
+                       // URL: /rest/v1/customers/{:id}
                        get(":id") {
                            it.json(customerService.fetch(it.pathParam("id").toInt()))
                        }
